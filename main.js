069242f4dbb0e9d049b2da14b495d9edb30335b4
lilux91 <lilualtamiran@gmail.com>
2022-09-04 16:50:00 -0300

    JavaScript

--- main.js
+++ main.js
@@ -5,8 +5,8 @@ let menuabierto=false
 iconMenu.addEventListener("click",()=>{
     
     if(menuabierto){ ulMenu.style.transform="translateX(200%)"
-    menuabierto=true
+    menuabierto=false
     } else {ulMenu.style.transform="translateX(0)"
-     menuabierto=false
+     menuabierto=true
     }
 })
